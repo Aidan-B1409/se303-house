1c94c79aaa65e439383a303256bc6770bb59d5ef
Aidan-B1409 <aidanb1049@gmail.com>
2021-04-15 14:42:25 -0700

    Refactoring - Condensed verse join logic into lambda function

--- lib/house.rb
+++ lib/house.rb
@@ -8,23 +8,19 @@ class House
   def line(num)
     lines = {
       1 => "the house that Jack built.\n",
-      2 => 'the malt that lay in ',
-      3 => 'the rat that ate ',
-      4 => 'the cat that killed ',
-      5 => 'the dog that worried ', 
-      6 => 'the cow with the crumpled horn that tossed ',
-      7 => 'the maiden all forlorn that milked ', 
-      8 => 'the man all tattered and torn that kissed ',
-      9 => 'the priest all shaven and shorn that married ',
-      10 => 'the rooster that crowed in the morn that woke ',
-      11 => 'the farmer sowing his corn that kept ',
-      12 => 'the horse and the hound and the horn that belonged to '
+      2 => 'the malt that lay in',
+      3 => 'the rat that ate',
+      4 => 'the cat that killed',
+      5 => 'the dog that worried', 
+      6 => 'the cow with the crumpled horn that tossed',
+      7 => 'the maiden all forlorn that milked', 
+      8 => 'the man all tattered and torn that kissed',
+      9 => 'the priest all shaven and shorn that married',
+      10 => 'the rooster that crowed in the morn that woke',
+      11 => 'the farmer sowing his corn that kept',
+      12 => 'the horse and the hound and the horn that belonged to'
     }
-    str = ''
-    num.downto(1).each do |n|
-      str << lines[n]
-    end
-    'This is ' << str
+    'This is ' << num.downto(1).collect { |x| lines[x] }.join(' ')
   end
 
   def recite()
