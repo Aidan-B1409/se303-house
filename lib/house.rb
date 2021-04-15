4908d40544452734bd70815749110540d588456b
Aidan-B1409 <aidanb1049@gmail.com>
2021-04-15 10:59:54 -0700

    Passing test 2 with a dict and iteration

--- lib/house.rb
+++ lib/house.rb
@@ -3,12 +3,18 @@ class House
   def initialize
   end
 
+
   def line(num)
-    case num
-    when 1
-      "This is the house that Jack built.\n"
-    when 2
-      "This is the malt that lay in the house that Jack built.\n"
+    lines = {
+      1 => "the house that Jack built.\n",
+      2 => 'the malt that lay in ',
+      3 => 'the rat that ate '
+    }
+    str = ''
+    num.downto(1).each do |n|
+      str << lines[n]
     end
+    puts str
+    'This is ' << str
   end
 end
