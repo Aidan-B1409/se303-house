9f58b17ba42409b17d7d05b6587712db4161f3b2
Aidan-B1409 <aidanb1049@gmail.com>
2021-04-15 11:01:45 -0700

    Passing test 6 - added to dict

--- lib/house.rb
+++ lib/house.rb
@@ -9,8 +9,9 @@ class House
       1 => "the house that Jack built.\n",
       2 => 'the malt that lay in ',
       3 => 'the rat that ate ',
-      4 => 'the cat that killed ', 
-      5 => 'the dog that worried '
+      4 => 'the cat that killed ',
+      5 => 'the dog that worried ', 
+      6 => 'the cow with the crumpled horn that tossed '
     }
     str = ''
     num.downto(1).each do |n|
