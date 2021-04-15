daac5701f21830d1d08d69c7e833edf296d3754a
Aidan-B1409 <aidanb1049@gmail.com>
2021-04-15 11:02:33 -0700

    Passing test 8 - added to dict

--- lib/house.rb
+++ lib/house.rb
@@ -11,8 +11,9 @@ class House
       3 => 'the rat that ate ',
       4 => 'the cat that killed ',
       5 => 'the dog that worried ', 
-      6 => 'the cow with the crumpled horn that tossed ', 
-      7 => 'the maiden all forlorn that milked '
+      6 => 'the cow with the crumpled horn that tossed ',
+      7 => 'the maiden all forlorn that milked ', 
+      8 => 'the man all tattered and torn that kissed '
     }
     str = ''
     num.downto(1).each do |n|
