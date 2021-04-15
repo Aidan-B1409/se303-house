cf353b49a80156e2999e3500447a27d9d43185d2
Aidan-B1409 <aidanb1049@gmail.com>
2021-04-15 11:20:18 -0700

    Passing up to test 12

--- lib/house.rb
+++ lib/house.rb
@@ -13,12 +13,20 @@ class House
       5 => 'the dog that worried ', 
       6 => 'the cow with the crumpled horn that tossed ',
       7 => 'the maiden all forlorn that milked ', 
-      8 => 'the man all tattered and torn that kissed '
+      8 => 'the man all tattered and torn that kissed ',
+      9 => 'the priest all shaven and shorn that married ',
+      10 => 'the rooster that crowed in the morn that woke ',
+      11 => 'the farmer sowing his corn that kept ',
+      12 => 'the horse and the hound and the horn that belonged to '
     }
     str = ''
     num.downto(1).each do |n|
       str << lines[n]
     end
     'This is ' << str
+  end
+
+  def recite()
+    return line(12)
   end
 end
