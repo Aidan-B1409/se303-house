a5aea8e018182210dcbda6336c9e1efe6eff47eb
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-25 13:37:02 -0700

    Passing test 2 by using number and if-else block

--- lib/house.rb
+++ lib/house.rb
@@ -4,8 +4,12 @@ class House
   def initialize()
   end
 
-  def line(_)
-    "This is the house that Jack built.\n"
+  def line(num)
+    if(num == 2)
+      "This is the malt that lay in the house that Jack built.\n"
+    else
+      "This is the house that Jack built.\n"
+    end
   end
 
   def recite()
