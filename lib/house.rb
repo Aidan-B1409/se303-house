b970e2fc3cfb7eddde977dff7c987a20cf839bf5
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-25 15:16:04 -0700

    Landing point - Horizontal refactoring for code smells

--- lib/house.rb
+++ lib/house.rb
@@ -21,7 +21,7 @@ class House
       'the farmer sowing his corn that kept',
       'the horse and the hound and the horn that belonged to'
     ]
-    prefix << (num-1).downto(0).collect { |x| ' ' << phrases[x]}.join(' ').squeeze(' ')
+    prefix << (num - 1).downto(0).collect { |x| ' ' << phrases[x]}.join(' ').squeeze(' ')
   end
 
   def recite
