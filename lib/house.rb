e1ad1ccfc8dd7c62fb88b9456f9944609754f5bc
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-25 14:40:56 -0700

    Fixed whitesepace

--- lib/house.rb
+++ lib/house.rb
@@ -6,35 +6,32 @@ class House
 
   def line(num)
     preface = 'This is the'
-    suffix = " house that Jack built.\n"
+    suffix = "house that Jack built.\n"
     line = case num
     when 12
-      " horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 11
-      " farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 10
-      " rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 9
-      " priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 8
-      " man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 7
-      " maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 6
-      " cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 5
-      " dog that worried the cat that killed the rat that ate the malt that lay in the"
+      "dog that worried the cat that killed the rat that ate the malt that lay in the"
     when 4
-      " cat that killed the rat that ate the malt that lay in the"
+      "cat that killed the rat that ate the malt that lay in the"
     when 3
-      " rat that ate the malt that lay in the"
+      "rat that ate the malt that lay in the"
     when 2
-      " malt that lay in the"
-    else
-      ''
+      "malt that lay in the"
     end
-    # preface << line << suffix
-    "#{preface}#{line}#{suffix}"
+    "#{preface} #{line} #{suffix}".squeeze(' ')
   end
 
   def recite
