2ff9a9a08725905eb0b76b75b3f705b196f3e64a
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-25 13:41:43 -0700

    Passing test 3, introducing case statement

--- lib/house.rb
+++ lib/house.rb
@@ -5,10 +5,15 @@ class House
   end
 
   def line(num)
-    if(num == 2)
-      "This is the malt that lay in the house that Jack built.\n"
+    case num
+    when 3
+      "This is the rat that ate the malt that lay in the house that Jack built.\n"
     else
-      "This is the house that Jack built.\n"
+      if(num == 2)
+        "This is the malt that lay in the house that Jack built.\n"
+      else
+        "This is the house that Jack built.\n"
+      end
     end
   end
 
