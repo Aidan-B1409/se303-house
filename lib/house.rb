0db5f3461b83a9abd87795327e1b78d39f9f36c9
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-25 14:51:01 -0700

    Phrase composition - Readying list

--- lib/house.rb
+++ lib/house.rb
@@ -2,11 +2,25 @@
 class House
 
   def initialize()
+
   end
 
   def line(num)
     preface = 'This is the'
     suffix = "house that Jack built.\n"
+
+    phrases = [
+      'the malt that lay in',
+      'the rat that ate',
+      'the cat that killed',
+      'the dog that worried',
+      'the cow with the crumpled horn that tossed',
+      'the maiden all forlorn that milked',
+      'the man all tattered and torn that kissed',
+      'the priest all shaven and shorn that married',
+      'the rooster that crowed in the morn that woke','the farmer sowing his corn that kept',
+    ]
+    
     line = case num
     when 12
       "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
