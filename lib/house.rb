aaf410cde1a3ea44477a1e0735c755364cdc790b
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-29 13:19:05 -0700

    Replace line method functionality with LineFactory call

--- lib/house.rb
+++ lib/house.rb
@@ -6,24 +6,24 @@ class House
   end
 
   def line(num)
-    prefix = 'This is'
-    phrases = [
-      "the house that Jack built.\n",
-      'the malt that lay in',
-      'the rat that ate',
-      'the cat that killed',
-      'the dog that worried',
-      'the cow with the crumpled horn that tossed',
-      'the maiden all forlorn that milked',
-      'the man all tattered and torn that kissed',
-      'the priest all shaven and shorn that married',
-      'the rooster that crowed in the morn that woke',
-      'the farmer sowing his corn that kept',
-      'the horse and the hound and the horn that belonged to'
-    ]
-    prefix << (num - 1).downto(0).collect { |x| ' ' << phrases[x]}.join(' ').squeeze(' ')
+    # prefix = 'This is'
+    # phrases = [
+    #   "the house that Jack built.\n",
+    #   'the malt that lay in',
+    #   'the rat that ate',
+    #   'the cat that killed',
+    #   'the dog that worried',
+    #   'the cow with the crumpled horn that tossed',
+    #   'the maiden all forlorn that milked',
+    #   'the man all tattered and torn that kissed',
+    #   'the priest all shaven and shorn that married',
+    #   'the rooster that crowed in the morn that woke',
+    #   'the farmer sowing his corn that kept',
+    #   'the horse and the hound and the horn that belonged to'
+    # ]
+    # prefix << (num - 1).downto(0).collect { |x| ' ' << phrases[x]}.join(' ').squeeze(' ')
 
-
+    @LineFactory.get_line(num)
   end
 
   def recite
@@ -49,11 +49,11 @@ class LineFactory
     'the farmer sowing his corn that kept',
     'the horse and the hound and the horn that belonged to'
     ]
-    
+
     @prefix = 'This is'
   end
 
   def get_line(num)
-    @prefix << (num - 1).downto(0).collect { |x| ' ' << @phrases[x]}.join(' ').squeeze(' ')
+    @prefix + (num - 1).downto(0).collect { |x| ' ' << @phrases[x]}.join(' ').squeeze(' ')
   end
 end
