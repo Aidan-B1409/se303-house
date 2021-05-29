8c9f250340a134bde1d5ab0f6115d775c0c5552c
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-29 13:19:18 -0700

    Removed duplicate code

--- lib/house.rb
+++ lib/house.rb
@@ -6,23 +6,6 @@ class House
   end
 
   def line(num)
-    # prefix = 'This is'
-    # phrases = [
-    #   "the house that Jack built.\n",
-    #   'the malt that lay in',
-    #   'the rat that ate',
-    #   'the cat that killed',
-    #   'the dog that worried',
-    #   'the cow with the crumpled horn that tossed',
-    #   'the maiden all forlorn that milked',
-    #   'the man all tattered and torn that kissed',
-    #   'the priest all shaven and shorn that married',
-    #   'the rooster that crowed in the morn that woke',
-    #   'the farmer sowing his corn that kept',
-    #   'the horse and the hound and the horn that belonged to'
-    # ]
-    # prefix << (num - 1).downto(0).collect { |x| ' ' << phrases[x]}.join(' ').squeeze(' ')
-
     @LineFactory.get_line(num)
   end
 
