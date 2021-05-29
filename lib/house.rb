6fdb2222a172b8655729ce8dc38ac36868ddd37c
Aidan-B1409 <aidanb1049@gmail.com>
2021-05-29 13:16:16 -0700

    Renamed Line to LineFactory

--- lib/house.rb
+++ lib/house.rb
@@ -2,7 +2,7 @@
 class House
 
   def initialize()
-
+    @LineFactory = LineFactory.new()
   end
 
   def line(num)
@@ -22,6 +22,8 @@ class House
       'the horse and the hound and the horn that belonged to'
     ]
     prefix << (num - 1).downto(0).collect { |x| ' ' << phrases[x]}.join(' ').squeeze(' ')
+
+
   end
 
   def recite
@@ -30,7 +32,7 @@ class House
 
 end
 
-class Line
+class LineFactory
 
   def initialize
     @phrases = [
